25083e468363afa35c5ac2a0c52326a18a4b683a
Tyuiu.LyaginVA@outlook.com <vova.lyagin.2016@mail.ru>
2023-10-20 18:51:23 +0500

    Task3

---
+++ Tyuiu.LyaginVA.Sprint3.Task3.V13.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LyaginVA.Sprint3.Task3.V13.Lib;
+
+namespace Tyuiu.LyaginVA.Sprint3.Task3.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidConvertStringToInt()
+        {
+            DataService ds = new DataService();
+
+            string str = "?sd!! 5gh. s!";
+            
+
+            double res = ds.ConvertStringToInt(str);
+
+            int wait = 5;
+
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.LyaginVA.Sprint3.Task3.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LyaginVA.Sprint3.Task3.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LyaginVA.Sprint3.Task3.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8bec5771-b355-4c7f-8c2e-798959ea83dd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
