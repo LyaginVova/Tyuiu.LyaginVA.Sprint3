d73d5430386a7999ae87884cacc7a440eec7abdb
Tyuiu.LyaginVA@outlook.com <vova.lyagin.2016@mail.ru>
2023-10-25 22:19:11 +0500

    Task5

--- Tyuiu.LyaginVA.Sprint3.Task5.V2/Program.cs
+++ Tyuiu.LyaginVA.Sprint3.Task5.V2/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.LyaginVA.Sprint3.Task4.V19.Lib;
+using Tyuiu.LyaginVA.Sprint3.Task5.V2.Lib;
 
-namespace Tyuiu.LyaginVA.Sprint3.Task4.V19
+namespace Tyuiu.LyaginVA.Sprint3.Task5.V2
 {
     class Program
     {
@@ -15,35 +15,39 @@ namespace Tyuiu.LyaginVA.Sprint3.Task4.V19
             Console.Title = "Спринт #3 | Выполнил: Лягин В. Л. | АСОиУБ-23-2";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #3                                                               *");
-            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
-            Console.WriteLine("* Задание #4                                                              *");
-            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
             Console.WriteLine("* Выполнил: Лягин Владимир Александрович | АСОиУБ-23-2                    *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
-            Console.WriteLine("* функции y=x/(cos(x) + x)+0.5. При х = 0 прервать цикл. Полученные       *");
-            Console.WriteLine("* значения перемножать.                                                   *");
+            Console.WriteLine("* Написать программу, которая вычисляет сумму сумм ряда по формуле, при   *");
+            Console.WriteLine("* x = 5.                                                                  *");
             Console.WriteLine("*                                                                         *");
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
             Console.WriteLine("***************************************************************************");
 
-            int startValue = -5;
-            int stopValue = 5;
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 12;
 
-            Console.WriteLine("Старт шага = " + startValue);
-            Console.WriteLine("Конец шага = " + stopValue);
+            Console.WriteLine("Переменная X = " + x);
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+            Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+            Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine("Произведение ряда = " + ds.Calculate(startValue, stopValue));
+            Console.WriteLine("Сумма сумм ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
 
             Console.ReadKey();
-
         }
     }
 }
